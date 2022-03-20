4f05bece08143b09bc15080bbe5fd380442a970d
飞行家 <705384983@qq.com>
2022-03-20 22:31:59 +0800

    短信设置

--- lilishop-master/lilishop-master/framework/src/main/java/cn/lili/common/properties/SmsTemplateProperties.java
+++ lilishop-master/lilishop-master/framework/src/main/java/cn/lili/common/properties/SmsTemplateProperties.java
@@ -11,26 +11,25 @@ import org.springframework.context.annotation.Configuration;
  */
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "lili.sms")
 public class SmsTemplateProperties {
     /**
      * 登录
      */
-    private String lOGIN = "SMS_205755300";
+    private String lOGIN = "您的短信验证码为：@";
     /**
      * 注册
      */
-    private String REGISTER = "SMS_205755298";
+    private String REGISTER = "您的短信验证码为：@";
     /**
      * 找回密码
      */
-    private String FIND_USER = "SMS_205755301";
+    private String FIND_USER = "您的短信验证码为：@";
     /**
      * 设置密码
      */
-    private String UPDATE_PASSWORD = "SMS_205755297";
+    private String UPDATE_PASSWORD = "您的短信验证码为：@";
     /**
      * 设置支付密码
      */
-    private String WALLET_PASSWORD = "SMS_205755297";
+    private String WALLET_PASSWORD = "您的短信验证码为：@";
 }
