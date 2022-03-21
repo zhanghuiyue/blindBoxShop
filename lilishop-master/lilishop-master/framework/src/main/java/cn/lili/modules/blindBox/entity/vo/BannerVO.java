985b62d95279594672ca2e3bff268d97125be472
飞行家 <705384983@qq.com>
2022-03-21 18:08:56 +0800

    盲盒推荐banner

---
+++ lilishop-master/lilishop-master/framework/src/main/java/cn/lili/modules/blindBox/entity/vo/BannerVO.java
@@ -0,0 +1,17 @@
+package cn.lili.modules.blindBox.entity.vo;
+
+import cn.lili.modules.blindBox.entity.dos.Banner;
+import lombok.Data;
+
+/**
+ * 品牌VO
+ *
+ * @author lilei
+ * @since 2022-03-21 15:24:13
+ */
+@Data
+public class BannerVO extends Banner {
+
+    private static final long serialVersionUID = 3829199991161122317L;
+
+}
