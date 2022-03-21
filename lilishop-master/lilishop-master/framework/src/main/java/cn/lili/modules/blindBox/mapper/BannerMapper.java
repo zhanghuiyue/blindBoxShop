d35b9cafd748c2859254eaffec62c88616bebdb4
飞行家 <705384983@qq.com>
2022-03-21 18:09:04 +0800

    盲盒推荐banner

---
+++ lilishop-master/lilishop-master/framework/src/main/java/cn/lili/modules/blindBox/mapper/BannerMapper.java
@@ -0,0 +1,16 @@
+package cn.lili.modules.blindBox.mapper;
+
+
+import cn.lili.modules.blindBox.entity.dos.Banner;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 盲盒推荐banner处理层
+ *
+ * @author lilei
+ * @since 2022-03-21 14:54:56
+ */
+public interface BannerMapper extends BaseMapper<Banner> {
+
+
+}
