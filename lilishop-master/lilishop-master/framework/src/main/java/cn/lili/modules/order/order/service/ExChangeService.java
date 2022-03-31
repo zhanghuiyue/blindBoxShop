930a6540430f6a0556fe1383744002fe8a76523b
飞行家 <705384983@qq.com>
2022-03-31 11:18:40 +0800

    兑换商城创建订单

---
+++ lilishop-master/lilishop-master/framework/src/main/java/cn/lili/modules/order/order/service/ExChangeService.java
@@ -0,0 +1,46 @@
+package cn.lili.modules.order.order.service;
+
+
+
+import cn.lili.modules.order.order.entity.dos.Order;
+import cn.lili.modules.order.order.entity.dto.ExChangeTradeDTO;
+import cn.lili.modules.order.order.entity.vo.ExChangeParams;
+
+/**
+ * 兑换商品业务层
+ *
+ * @author Chopper
+ * @since 2020-03-23 12:29 下午
+ */
+public interface ExChangeService {
+
+
+    /**
+     * 兑换池加入一个商品
+     *
+     * @param skuId    要写入的skuId
+     * @param num      要加入兑换池的数量
+     */
+    void add(String skuId, Integer num);
+
+
+    /**
+     * 创建交易
+     * 1.获取购物车类型，不同的购物车类型有不同的订单逻辑
+     * 购物车类型：购物车、立即购买、虚拟商品、拼团、积分
+     * 2.校验用户的收件人信息
+     * 3.设置交易的基础参数
+     * 4.交易信息存储到缓存中
+     * 5.创建交易
+     * 6.清除购物车选择数据
+     *
+     * @param  exChangeParams 创建交易参数
+     * @return 交易信息
+     */
+    Order createExChangeTrade(ExChangeParams exChangeParams);
+
+
+
+
+
+}
