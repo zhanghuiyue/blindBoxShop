b533503379357f0f51614bede9f23858ff4c4cd2
飞行家 <705384983@qq.com>
2022-04-01 11:39:08 +0800

    兑换商城提交订单

--- lilishop-master/lilishop-master/framework/src/main/java/cn/lili/modules/order/order/service/ExChangeService.java
+++ lilishop-master/lilishop-master/framework/src/main/java/cn/lili/modules/order/order/service/ExChangeService.java
@@ -2,7 +2,9 @@ package cn.lili.modules.order.order.service;
 
 
 
+import cn.lili.modules.order.cart.entity.vo.TradeParams;
 import cn.lili.modules.order.order.entity.dos.Order;
+import cn.lili.modules.order.order.entity.dos.Trade;
 import cn.lili.modules.order.order.entity.dto.ExChangeTradeDTO;
 import cn.lili.modules.order.order.entity.vo.ExChangeParams;
 
@@ -20,26 +22,27 @@ public interface ExChangeService {
      *
      * @param skuId    要写入的skuId
      * @param num      要加入兑换池的数量
+     * @param cartType 兑换池类型
+     * @param cover    是否覆盖兑换池的数量，如果为否则累加，否则直接覆盖
      */
-    void add(String skuId, Integer num);
+    void add(String skuId, Integer num, String cartType, Boolean cover);
+
 
 
     /**
      * 创建交易
-     * 1.获取购物车类型，不同的购物车类型有不同的订单逻辑
-     * 购物车类型：购物车、立即购买、虚拟商品、拼团、积分
+     * 1.获取兑换池类型，不同的兑换池类型有不同的订单逻辑
+     * 兑换池类型：兑换池、立即购买、虚拟商品、拼团、积分
      * 2.校验用户的收件人信息
      * 3.设置交易的基础参数
      * 4.交易信息存储到缓存中
      * 5.创建交易
-     * 6.清除购物车选择数据
+     * 6.清除兑换池选择数据
      *
-     * @param  exChangeParams 创建交易参数
+     * @param tradeParams 创建交易参数
      * @return 交易信息
      */
-    Order createExChangeTrade(ExChangeParams exChangeParams);
-
-
+    Trade createTrade(TradeParams tradeParams);
 
 
 
